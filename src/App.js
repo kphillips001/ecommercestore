ce3a1b80912317ab1e84a9f4fae2f46bd3f0c360
Kevin <knphillips01@gmail.com>
2019-08-23 17:36:37 -0500

    added homepage and directory components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import HomePage from './homepage'
+import HomePage from './pages/homepage/homepage'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <HomePage />
-    </div>
+      </div>
     );
   }
 }
