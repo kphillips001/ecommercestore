cbc93f060d4d2b9d1da43701d0a6249ae767c23c
Kevin <knphillips01@gmail.com>
2019-08-24 16:26:13 -0500

    Created collection-preview and collection-item components for the shop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop'; 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App () {
  return (
       <div className="App">
         <Switch>
           <Route exact path= '/' component={HomePage}  />
-          <Route path= '/hats' component={HomePage}  />
+          <Route path= '/shop' component={ShopPage}  />
         </Switch>
       </div>
     );
