793aadca9aa1e1a4bdb020c424d91f2557f153f1
Kevin <knphillips01@gmail.com>
2019-08-24 21:48:39 -0500

    Upodated app with collection preview and collitem item component

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item';
 import './collection-preview.scss';
 
-const CollectonPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
 
-export default CollectonPreview;
+export default CollectionPreview;
