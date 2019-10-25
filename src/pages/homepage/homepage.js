e1d319e19bbfa265413aa27c295e250763857b80
Kevin <knphillips01@gmail.com>
2019-10-25 10:49:41 -0500

    update repo name

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Directory from '../../components/directory/directory'
-import './homepage.scss';
+import './homepage.scss'
+ 
 
 const HomePage = () => (
   <div className='homepage'>
     <Directory />
-  </div>  
+    </div>
 )
 
 export default HomePage; 
